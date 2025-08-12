97872e619923672abc3414724e3275f52d05c4e7
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-08-12 14:44:14 +0300

    style: apply minor formatting and code improvements
    - improve code readability
    - fix typo errors

--- php/day-4/assets/index.js
+++ php/day-4/assets/index.js
@@ -6,40 +6,40 @@ const nameParagraph = document.createElement("p");
 
 // add some delay
 setTimeout(() => {
-	alert("Welcome to my website");
+  alert("Welcome to my website");
 
-	const username = prompt("Enter your name");
-	nameParagraph.textContent = `Welcome ${username}`;
-	appDiv.appendChild(nameParagraph);
+  const username = prompt("Enter your name");
+  nameParagraph.textContent = `Welcome ${username}`;
+  appDiv.appendChild(nameParagraph);
 }, 500);
 
 // --- Task Two --- //
 function sum(num1, num2) {
-	const n1 = parseInt(num1);
-	const n2 = parseInt(num2);
+  const n1 = Number.parseInt(num1);
+  const n2 = Number.parseInt(num2);
 
-	return n1 + n2;
+  return n1 + n2;
 }
 
 function handleSumClick() {
-	const num1 = prompt("Enter first number");
-	const num2 = prompt("Enter second number");
+  const num1 = prompt("Enter first number");
+  const num2 = prompt("Enter second number");
 
-	const sumParagraph = document.createElement("p");
-	sumParagraph.textContent = `sum of ${num1} + ${num2} = `;
-	sumParagraph.textContent += sum(num1, num2);
+  const sumParagraph = document.createElement("p");
+  sumParagraph.textContent = `sum of ${num1} + ${num2} = `;
+  sumParagraph.textContent += sum(num1, num2);
 
-	appDiv.appendChild(sumParagraph);
+  appDiv.appendChild(sumParagraph);
 }
 
 // --- Task Three --- //
 
 function handleMathClick() {
-	const input = prompt("Enter a math expression");
-	const result = eval(input);
+  const input = prompt("Enter a math expression");
+  const result = eval(input);
 
-	const mathParagraph = document.createElement("p");
-	mathParagraph.textContent = `You entered: "${input}", and the result is: ${result}`;
+  const mathParagraph = document.createElement("p");
+  mathParagraph.textContent = `You entered: "${input}", and the result is: ${result}`;
 
-	appDiv.appendChild(mathParagraph);
+  appDiv.appendChild(mathParagraph);
 }
