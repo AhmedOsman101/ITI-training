693dcc6054e0bdbbc925d873b16e8c2f1d5148ce
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-08-25 14:11:51 +0300

    refactor(php): improve code readability and structure
    - add editor line numbers
    - move body start and end to utils
    - declare strict types in utils file

--- php/day-12/index.php
+++ php/day-12/index.php
@@ -1,119 +1,88 @@
-<?php declare(strict_types=1); ?>
+<?php declare(strict_types=1);
 
-<!DOCTYPE html>
-<html lang="en">
+require 'vendor/autoload.php';
+require_once __DIR__ . '/../utils.php';
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Day 12</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" />
-  <style>
-    body {
-      min-height: 100vh;
-      max-width: 1000px;
-    }
+bodyStart("Day 12");
+heading(3, "1. Print 'Welcome to php'");
+println("Welcome to php");
 
-    pre>code {
-      font-size: 1rem;
-      padding-left: 1.2rem;
-    }
+$x = 5;
+$y = 'Welcome';
+$z = true;
 
-    td {
-      border-bottom: 1px solid #526980;
-    }
-  </style>
-</head>
+heading(3, "2, 3, 6. Display the type of each variable");
+println(
+  gettype($x),
+  gettype($y),
+  gettype($z),
+);
 
-<body>
+heading(3, "method 1 (for loop)");
+preStart();
+for ($i = 0; $i <= 15; $i++) echo "$i ";
+preEnd();
 
+heading(3, "method 2 (while loop)");
+preStart();
 
-  <?php
+$i = 0;
+while ($i <= 15) {
+  echo "$i ";
+  $i++;
+}
+preEnd();
 
-  require 'vendor/autoload.php';
-  require_once __DIR__ . '/../utils.php';
+// 5. define a constant
+const INSTITUTE = 'ITI';
 
-  heading(3, "1. Print 'Welcome to php'");
-  println("Welcome to php");
+// 7. isset
+println(
+  isset($x),
+  isset($y),
+  isset($z),
+);
 
-  $x = 5;
-  $y = 'Welcome';
-  $z = true;
+// 8. empty
+println(
+  empty($x),
+  empty($y),
+  empty($z),
+);
 
-  heading(3, "2, 3, 6. Display the type of each variable");
-  println(
-    gettype($x),
-    gettype($y),
-    gettype($z),
-  );
+// 9. add m and n
+$result = 5 + 6;
+println($result > 50 ? 'Accepted' : 'Not accepted');
 
-  heading(3, "method 1 (for loop)");
-  preStart();
-  for ($i = 0; $i <= 15; $i++) echo "$i ";
-  preEnd();
+// 11. number to string
+function numberToString(int|float $number): string {
+  return (string) $number;
+  // or return strval($number)
+  // or return "$number"
+}
+?>
 
-  heading(3, "method 2 (while loop)");
-  preStart();
+<?php $data = [
+  ["name" => "A", "salary" => 1000],
+  ["name" => "B", "salary" => 1200],
+  ["name" => "C", "salary" => 1400],
+] ?>
 
-  $i = 0;
-  while ($i <= 15) {
-    echo "$i ";
-    $i++;
-  }
-  preEnd();
-
-  // 5. define a constant
-  const INSTITUTE = 'ITI';
-
-  // 7. isset
-  println(
-    isset($x),
-    isset($y),
-    isset($z),
-  );
-
-  // 8. empty
-  println(
-    empty($x),
-    empty($y),
-    empty($z),
-  );
-
-  // 9. add m and n
-  $result = 5 + 6;
-  println($result > 50 ? 'Accepted' : 'Not accepted');
-
-  // 11. number to string
-  function numberToString(int|float $number): string {
-    return (string) $number;
-    // or return strval($number)
-    // or return "$number"
-  }
-  ?>
-
-  <?php $data = [
-    ["name" => "A", "salary" => 1000],
-    ["name" => "B", "salary" => 1200],
-    ["name" => "C", "salary" => 1400],
-  ] ?>
-
-  <table>
-    <thead>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Salary</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php for ($i = 0; $i < count($data); $i++): ?>
+      <?php $user = $data[$i] ?>
       <tr>
-        <th>Name</th>
-        <th>Salary</th>
+        <td>Salary for Mr. <?= $user["name"] ?></td>
+        <td><?= $user["salary"] ?>$</td>
       </tr>
-    </thead>
-    <tbody>
-      <?php for ($i = 0; $i < count($data); $i++): ?>
-        <?php $user = $data[$i] ?>
-        <tr>
-          <td>Salary for Mr. <?= $user["name"] ?></td>
-          <td><?= $user["salary"] ?>$</td>
-        </tr>
-      <?php endfor; ?>
-    </tbody>
-  </table>
-</body>
-
-</html>
+    <?php endfor; ?>
+  </tbody>
+</table>
+<?php bodyEnd();
--- php/utils.php
+++ php/utils.php
@@ -1,6 +1,5 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types=1);
 
 function stringify(mixed $arg): string {
   if ($arg === null) return 'null';
@@ -44,4 +43,41 @@ function tag(string $tag, string|int|bool $content): void {
 
 function heading(int $level, string|int|bool $content): void {
   tag("h$level", $content);
+}
+
+function bodyStart(string $title = "Title", string $styles = "") {
+  echo <<<EOF
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>$title</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" />
+  <style>
+    body {
+      min-height: 100vh;
+      max-width: 1000px;
+    }
+
+    pre>code {
+      font-size: 1rem;
+      padding-left: 1.2rem;
+    }
+
+    td {
+      border-bottom: 1px solid #526980;
+    }
+
+    $styles
+  </style>
+</head>
+
+<body>
+EOF;
+}
+
+function bodyEnd() {
+  echo "\n</body>\n</html>\n";
 }
