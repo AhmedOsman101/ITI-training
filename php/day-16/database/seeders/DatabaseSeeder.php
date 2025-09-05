e05d4634f064f9a22bce876362e6e1ff4c719e18
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-09-05 14:27:59 +0300

    feat: add department model, controller, migration
    - create department model
    - create department controller
    - create department migration

--- php/day-16/database/seeders/DatabaseSeeder.php
+++ php/day-16/database/seeders/DatabaseSeeder.php
@@ -2,24 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\Student;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void {
+    User::create([
+      'name'     => "Admin",
+      'email'    => 'admin@example.com',
+      'password' => Hash::make('password'),
+    ]);
 
-        User::factory()->create(
-            [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            ]
-        );
-    }
+    Department::factory()->count(10)->create();
+    Student::factory()->count(3)->create();
+  }
 }
